f0fdc5e0c8a03cce16ccc9ae20049a9ff5262760
Johnny-Dean <johnnydpalacio@gmail.com>
2025-02-23 11:39:47 -0800

    Move `db_connector` logic to main

--- root/backend/main.py
+++ root/backend/main.py
@@ -1,13 +1,33 @@
 from fastapi import FastAPI
-
-from .db_connector import DatabaseConnection
+from models.models import Users
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import Session
 
 app = FastAPI()
-db_connector = DatabaseConnection(
-    connection_string='postgresql://postgres:123@postgres_db:5432/pickwisely_dev'
-    )
+
+db_url = URL.create(
+     drivername="postgresql",
+     username="postgres",
+     password="123", 
+     host="postgres_db",
+     database="pickwisely_dev",
+     port=5432
+ )
+
+engine = create_engine(db_url)
+session = Session(engine)
 
 @app.get("/")
 async def root():
-    all_users = db_connector.execute_dummy_query()
-    return all_users
+    return {"message": "Hello World"}
+
+@app.get("/users")
+def get_all_users():
+    users_query = session.query(Users)
+    return users_query.all()
+
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: int):
+    user_query = session.query(Users).filter(Users.user_id == user_id)
+    return user_query.first()

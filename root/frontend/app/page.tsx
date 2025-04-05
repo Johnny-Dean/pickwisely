4ad23f92790f898f2e64687702d23ee94ec7816f
maddytwang <52714621+maddytwang@users.noreply.github.com>
2025-04-05 16:49:57 -0700

    UI Configurations: Homepage and user pages

--- root/frontend/app/page.tsx
+++ root/frontend/app/page.tsx
@@ -1,16 +1,30 @@
+// Landing Page
+"use client";
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './globals.css'
 
 export default function Home() {
+  const [initial, setInitial] = useState(0)
+
+  useEffect(() => {
+    setInitial(initial => initial + 1)
+  }, [])
+
   return (
-    <div>
-      <h1 className="flex items-center text-5xl font-extrabold dark:text-white">
-        pickwisely.ai
-      </h1>
-      <Link href = "/users">
-      <button color="primary">Begin</button>
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      {/* typing effect container */}
+      <div className="typing-container">
+        {/* TODO: Fix animation */}
+        <h1 className="text-5xl font-body overflow-hidden whitespace-nowrap border-r-4 border-white">
+          pickwisely.ai
+        </h1>
+      </div>
+      
+      <Link href="/users">
+        <button className="px-6 py-3 mt-4 hover:opacity-20">Begin</button>
       </Link>
     </div>
-  )
+  );
 }
--- root/frontend/app/users/new/page.tsx
+++ root/frontend/app/users/new/page.tsx
@@ -1,9 +1,52 @@
+"use client";
+
 import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { Input } from "@material-tailwind/react";
 
 const newUser = () => {
-  return (
-    <div>newUser</div>
-  )
+
+  const url = "http://localhost:8000/users/"
+  const promise = axios.get(url)
+
+  promise
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err.message))
+
+  const [user, setUser] = useState([])
+
+  const addUser = (event) => {
+    event.preventDefault()
+    // console.log('user added', event.target)
+
+    const userObject = {
+      user_id: 1, 
+      username: event.target.value, 
+      role: 'user', 
+      created_at: Date.now(),
+      email: ""
+    }
+    console.log(userObject.username)
+    axios.post(url, userObject).then(response => {console.log(response)})
+  }
+
+  const userValidation = (userInfo) => {
+    // check if username exists already in database 
+    // check if email is valid
+  }
+
+  
+    return (
+      <div>
+        <form onSubmit={addUser}>
+        <input 
+        className="flex w-72 outline-solid "
+        name="userName"/>
+        <button className="outline-solid" type="submit">Begin</button>
+      </form>
+      </div>
+    )
 }
 
-export default newUser
+export default newUser;
